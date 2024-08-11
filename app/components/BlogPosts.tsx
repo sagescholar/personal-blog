9ba51a9aa3dc6a1f034d80a2086cf908a28c6df6
sagescholar <syota.kushizaki@gmail.com>
2024-08-12 03:33:02 +0900

    配置変更

--- app/components/BlogPosts.tsx
+++ app/components/BlogPosts.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState, useEffect } from 'react'
-import { supabase } from '../lib/supabase'
+import { supabase } from '../../lib/supabase'
 import Link from 'next/link'
 
 // 投稿の型を定義
