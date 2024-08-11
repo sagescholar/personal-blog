4af788f49e30ea942468fe10336410a4c5f74471
sagescholar <syota.kushizaki@gmail.com>
2024-08-12 03:16:50 +0900

    link修正

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,6 +1,6 @@
 import PostContent from '../../components/PostContent'
 
-export default function Post({ params }) {
+export default function Post({ params }: { params: { id: string } }) {
   return (
     <div className="container mx-auto px-4">
       <PostContent id={params.id} />
